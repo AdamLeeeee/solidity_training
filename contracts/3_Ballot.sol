e2dfa011ae61d5705e90080a1c406ab7ee8380da
AdamLeeeee <ly1130399693@gmail.com>
2024-09-30 23:51:45 +0800

    add time vaild in set function

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -1,19 +1,25 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.20;
+pragma solidity >=0.7.0 <0.9.0;
 
+/** 
+ * @title Ballot
+ * @dev Implements voting process along with vote delegation
+ */
 contract Ballot {
 
     struct Voter {
-        uint weight;
-        bool voted;
-        address delegate;
-        uint vote;
+        uint weight; // weight is accumulated by delegation
+        bool voted;  // if true, that person already voted
+        address delegate; // person delegated to
+        uint vote;   // index of the voted proposal
     }
 
     struct Proposal {
-        bytes32 name;   
-        uint voteCount; 
+        // If you can limit the length to a certain number of bytes, 
+        // always use one of bytes1 to bytes32 because they are much cheaper
+        bytes32 name;   // short name (up to 32 bytes)
+        uint voteCount; // number of accumulated votes
     }
 
     address public chairperson;
@@ -22,17 +28,18 @@ contract Ballot {
 
     Proposal[] public proposals;
 
-    uint public startTime;
-    
-    uint public endTime;
-
-    constructor(bytes32[] memory proposalNames, uint _startTime, uint _endTime) {
-        startTime = _startTime;
-        endTime = _endTime;
+    /** 
+     * @dev Create a new ballot to choose one of 'proposalNames'.
+     * @param proposalNames names of proposals
+     */
+    constructor(bytes32[] memory proposalNames) {
         chairperson = msg.sender;
         voters[chairperson].weight = 1;
 
         for (uint i = 0; i < proposalNames.length; i++) {
+            // 'Proposal({...})' creates a temporary
+            // Proposal object and 'proposals.push(...)'
+            // appends it to the end of 'proposals'.
             proposals.push(Proposal({
                 name: proposalNames[i],
                 voteCount: 0
@@ -40,6 +47,10 @@ contract Ballot {
         }
     }
 
+    /** 
+     * @dev Give 'voter' the right to vote on this ballot. May only be called by 'chairperson'.
+     * @param voter address of voter
+     */
     function giveRightToVote(address voter) public {
         require(
             msg.sender == chairperson,
@@ -53,6 +64,10 @@ contract Ballot {
         voters[voter].weight = 1;
     }
 
+    /**
+     * @dev Delegate your vote to the voter 'to'.
+     * @param to address to which vote is delegated
+     */
     function delegate(address to) public {
         Voter storage sender = voters[msg.sender];
         require(!sender.voted, "You already voted.");
@@ -61,6 +76,7 @@ contract Ballot {
         while (voters[to].delegate != address(0)) {
             to = voters[to].delegate;
 
+            // We found a loop in the delegation, not allowed.
             require(to != msg.sender, "Found loop in delegation.");
         }
         sender.voted = true;
@@ -77,18 +93,27 @@ contract Ballot {
         }
     }
 
+    /**
+     * @dev Give your vote (including votes delegated to you) to proposal 'proposals[proposal].name'.
+     * @param proposal index of proposal in the proposals array
+     */
     function vote(uint proposal) public {
-        require(block.timestamp >= startTime && block.timestamp < endTime, "Voting time not started or ended.");
-
         Voter storage sender = voters[msg.sender];
         require(sender.weight != 0, "Has no right to vote");
         require(!sender.voted, "Already voted.");
         sender.voted = true;
         sender.vote = proposal;
 
+        // If 'proposal' is out of the range of the array,
+        // this will throw automatically and revert all
+        // changes.
         proposals[proposal].voteCount += sender.weight;
     }
 
+    /** 
+     * @dev Computes the winning proposal taking all previous votes into account.
+     * @return winningProposal_ index of winning proposal in the proposals array
+     */
     function winningProposal() public view
             returns (uint winningProposal_)
     {
@@ -101,19 +126,13 @@ contract Ballot {
         }
     }
 
+    /** 
+     * @dev Calls winningProposal() function to get the index of the winner contained in the proposals array and then
+     * @return winnerName_ the name of the winner
+     */
     function winnerName() public view
             returns (bytes32 winnerName_)
     {
         winnerName_ = proposals[winningProposal()].name;
-    }
-
-    function setVoterWeight(address voter, uint weight) public {
-        require(block.timestamp >= startTime && block.timestamp < endTime, "Voting time not started or ended.");
-
-        require(msg.sender == chairperson, "Only chairperson can set voter weights.");
-        
-        require(!voters[voter].voted, "The voter already voted.");
-        
-        voters[voter].weight = weight;
     }
 }
